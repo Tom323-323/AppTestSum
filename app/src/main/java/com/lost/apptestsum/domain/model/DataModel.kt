242fe96ea231b5cf72c496a211ccf04d0debb840
Tom323-323 <64865370+Tom323-323@users.noreply.github.com>
2022-01-30 17:16:46 +0300

    Clean Archit part2

--- app/src/main/java/com/lost/apptestsum/domain/model/DataModel.kt
+++ app/src/main/java/com/lost/apptestsum/domain/model/DataModel.kt
@@ -1,6 +1,6 @@
 package com.lost.apptestsum.domain.model
 
-import android.text.Editable
+
 
 class DataModel(var data_text: String, var data_day:String){
 }
--- app/src/main/java/com/lost/apptestsum/presentation/RVdata.kt
+++ app/src/main/java/com/lost/apptestsum/presentation/RVdata.kt
@@ -1,4 +1,12 @@
 package com.lost.apptestsum.presentation
 
-class RVdata {
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.lost.apptestsum.R
+
+class RVdata : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rvdata)
+    }
 }
