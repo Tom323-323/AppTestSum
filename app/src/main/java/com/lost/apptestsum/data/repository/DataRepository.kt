ca392c4211d37730be934106573d650246782a59
Tom323-323 <64865370+Tom323-323@users.noreply.github.com>
2022-02-07 21:10:02 +0300

    07.02.2022 - DB FireBase add DataModel

--- app/src/main/java/com/lost/apptestsum/data/repository/DataRepository.kt
+++ app/src/main/java/com/lost/apptestsum/data/repository/DataRepository.kt
@@ -1,24 +1,27 @@
 package com.lost.apptestsum.data.repository
 
-
-import android.content.Context
 import android.util.Log
-import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import com.lost.apptestsum.domain.model.DataModel
 import com.lost.apptestsum.domain.repository.DataRepository
 
-private lateinit var data: DatabaseReference
 
 
-class DataRepositoryImp(private val context: Context) : DataRepository {
 
-    val KEY: String = "key_data"
+class DataRepositoryImp : DataRepository {
+
+    private val database = Firebase.database
+    private val KEY: String = "key_data"
 
     override fun saveData(saveParam: DataModel){
         val text = saveParam.data_day
-        Log.d("AAD", text)
-        data = FirebaseDatabase.getInstance().getReference(KEY)
+        val text2 = saveParam.data_text
+        val dataFireModel = DataModel(data_text = text2,data_day = text)
+        val dataFire = database.getReference("DataHolder")
+        dataFire.setValue(dataFireModel)
+
 
 
     }
