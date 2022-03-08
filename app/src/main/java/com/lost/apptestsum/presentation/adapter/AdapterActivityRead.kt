da8906c485757d86031a28445a77a62d62ef65f6
Tom323-323 <64865370+Tom323-323@users.noreply.github.com>
2022-03-08 20:18:36 +0300

    08.03.2022 - Problem delete item in recyclerview. Delete in FB and in RV...

--- app/src/main/java/com/lost/apptestsum/presentation/adapter/AdapterActivityRead.kt
+++ app/src/main/java/com/lost/apptestsum/presentation/adapter/AdapterActivityRead.kt
@@ -3,32 +3,41 @@ package com.lost.apptestsum.presentation.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.lost.apptestsum.R
 import com.lost.apptestsum.domain.model.DataModel
 
-class AdapterActivityRead(private val dataList: ArrayList<DataModel>): RecyclerView.Adapter<AdapterActivityRead.ViewHolder>() {
-    override fun onCreateViewHolder(
-        parent: ViewGroup,
-        viewType: Int
-    ): AdapterActivityRead.ViewHolder {
+class AdapterActivityRead(private val dataList: ArrayList<DataModel>,val onClickDelete:(Int)->Unit): RecyclerView.Adapter<AdapterActivityRead.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AdapterActivityRead.ViewHolder {
         val itemView = LayoutInflater.from(parent.context).inflate(R.layout.view_holder,parent,false)
         return ViewHolder(itemView)
     }
 
     override fun onBindViewHolder(holder: AdapterActivityRead.ViewHolder, position: Int) {
-       val data: DataModel = dataList[position]
-        holder.data_text.text = data.data_text
-        holder.day_text.text = data.data_day
+       holder.bind(dataList[position],position)
+
 
     }
 
     override fun getItemCount(): Int {
         return dataList.size
     }
-        class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
-            val data_text: TextView = itemView.findViewById(R.id.data_text)
-            val day_text: TextView = itemView.findViewById(R.id.day_text)
+        inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+
+            fun bind(dataModel: DataModel, index:Int){
+                val data_text: TextView = itemView.findViewById(R.id.data_text)
+                val day_text: TextView = itemView.findViewById(R.id.day_text)
+                val btn_delete: Button = itemView.findViewById(R.id.btn_delete)
+
+                data_text.text = dataModel.data_text
+                day_text.text = dataModel.data_day
+
+                btn_delete.setOnClickListener{onClickDelete(index)}
+            }
+
+
         }
 }
