b92c017a901490e06a8a0ea7c14a3eddb1f3d218
Tom323-323 <64865370+Tom323-323@users.noreply.github.com>
2022-03-14 22:11:57 +0300

    14.03.2022 - UserRepositoryImp.kt part 2.

--- domain/src/main/java/com/lost/apptestsum/domain/usecase/Registr.kt
+++ domain/src/main/java/com/lost/apptestsum/domain/usecase/Registr.kt
@@ -1,12 +1,13 @@
 package com.lost.apptestsum.domain.usecase
 
 import com.lost.apptestsum.domain.model.UserRegModel
+import com.lost.apptestsum.domain.repository.UserRepository
 
-class Registr {
+class Registr (private val userRepository: UserRepository){
 
     fun registration(objUserReg: UserRegModel){
         val objUserReg1 = UserRegModel(mail = objUserReg.mail, password = objUserReg.password)
-
+        userRepository.registr(objUserReg1)
     }
 
 }
--- domain/src/main/java/com/lost/apptestsum/domain/usecase/Sign.kt
+++ domain/src/main/java/com/lost/apptestsum/domain/usecase/Sign.kt
@@ -1,11 +1,12 @@
 package com.lost.apptestsum.domain.usecase
 
 import com.lost.apptestsum.domain.model.UserRegModel
+import com.lost.apptestsum.domain.repository.UserRepository
 
-class Sign {
+class Sign (private val userRepository: UserRepository){
 
     fun sign_in(objUserSign: UserRegModel) {
         val objUserSign1 = UserRegModel(mail = objUserSign.mail, password = objUserSign.password)
-
+        userRepository.sign(objUserSign1)
     }
 }
