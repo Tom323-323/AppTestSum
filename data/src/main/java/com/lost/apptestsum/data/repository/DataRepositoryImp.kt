d329b7a301755c1ad4a2b5c15c57da784d1c3588
Tom323-323 <64865370+Tom323-323@users.noreply.github.com>
2022-04-28 21:28:16 +0300

    28.04.2022 - Add Room DB. Part 2. Get data from DataRepositoryImp.kt

--- data/src/main/java/com/lost/apptestsum/data/repository/DataRepositoryImp.kt
+++ data/src/main/java/com/lost/apptestsum/data/repository/DataRepositoryImp.kt
@@ -2,14 +2,19 @@ package com.lost.apptestsum.data.repository
 
 import com.lost.apptestsum.data.storage.model.DataModelStorage
 import com.lost.apptestsum.data.storage.fireBase.FBstorage
+import com.lost.apptestsum.data.storage.room.DataBase
 import com.lost.apptestsum.domain.model.DataModel
 import com.lost.apptestsum.domain.repository.DataRepository
 
 
-class DataRepositoryImp (private val fBstorage: FBstorage): DataRepository {
+class DataRepositoryImp (private val fBstorage: FBstorage, private val dataRoom: DataBase): DataRepository {
 
     override fun saveData(saveParam: DataModel){
         return fBstorage.saveDataStorage(mapToStorage(saveParam))
+    }
+
+    fun saveDataRoom(saveParam: DataModel){
+        return dataRoom.saveDataStorage(mapToStorage(saveParam))
     }
 
     fun mapToStorage(saveParam: DataModel):DataModelStorage{
--- data/src/main/java/com/lost/apptestsum/data/storage/room/entity/DataEntity.kt
+++ data/src/main/java/com/lost/apptestsum/data/storage/room/entity/DataEntity.kt
@@ -1,4 +1,14 @@
 package com.lost.apptestsum.data.storage.room.entity
 
-class DataEntity {
-}
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity
+data class DataEntity(
+        @PrimaryKey val dataID: Int,
+        @ColumnInfo(name = "data_day") val data_day: String,
+        @ColumnInfo(name = "data_text") val data_text: String
+
+        )
